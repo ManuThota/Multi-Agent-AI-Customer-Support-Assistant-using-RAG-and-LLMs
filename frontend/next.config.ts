bebd83c25301a4a0a4393194fa2639201d19303a
Manu_i_003 <manikantathota2003@gmail.com>
2026-07-13 17:33:18 +0530

    feat: initialize frontend project with Next.js configuration and dependencies

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
+import path from 'path';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  turbopack: {
+    // Locks module resolution to the frontend folder only
+    root: path.join(__dirname),
+  },
 };
 
 export default nextConfig;
